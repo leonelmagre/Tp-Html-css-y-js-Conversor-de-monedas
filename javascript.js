64a7f3a9b52d79dc5aaadb8237df30998472734b
leonelmagre <50115148+leonelmagre@users.noreply.github.com>
2019-08-07 17:20:01 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,198 @@
+
+var euro = new moneda()
+euro.nombre ="euro"
+euro.tipodecambio= 0.9
+
+var libra = new moneda()
+libra.nombre ="libra"
+libra.tipodecambio= 0.82
+
+var peso = new moneda()
+peso.nombre ="peso"
+peso.tipodecambio= 44.5
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto dolares){
+		resultado = monto * tipodecambio
+	var montodolares = document.getElementById('Monto dolares').value
+	euro.convertir('montodolares')
+	
+}
+   }
+
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto dolares){
+		resultado = monto * tipodecambio
+	var montodolares = document.getElementById('Monto dolares').value
+	peso.convertir('montodolares')
+	
+}  
+} 
+
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto dolares){
+		resultado = monto * tipodecambio
+	var montodolares = document.getElementById('Monto dolares').value
+	libra.convertir('montodolares')
+	
+}  
+}
+
+
+var dolar= new moneda()
+dolar.nombre ="dolar"
+dolar.tipodecambio= 1.1
+
+var libra2 = new moneda()
+libra.nombre ="libra"
+libra.tipodecambio= 0.92
+
+var peso2 = new moneda()
+peso.nombre ="peso"
+peso.tipodecambio= 49.53
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto Euros){
+		resultado = monto * tipodecambio
+	var montoeuros = document.getElementById('Monto euros').value
+	dolar.convertir('montoeuros')
+	
+}   
+}
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto Euros){
+		resultado = monto * tipodecambio
+	var montoeuros = document.getElementById('Monto euros').value
+	peso.convertir('montoeuros')
+	
+}  
+ }
+
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto Euros){
+		resultado = monto * tipodecambio
+	var montoeuros = document.getElementById('Monto euros').value
+	libra.convertir('montoeuros')
+	
+}  
+ }
+
+
+
+
+var euro2 = new moneda()
+euro.nombre ="euro"
+euro.tipodecambio= 0.02
+
+var libra3 = new moneda()
+libra.nombre ="libras"
+libra.tipodecambio= 0.018
+
+var dolar2 = new moneda()
+dolar.nombre ="dolar"
+dolar.tipodecambio= 0.022
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto Pesos){
+		resultado = monto * tipodecambio
+	var montopesos = document.getElementById('Monto pesos').value
+	dolar.convertir('montopesos')
+	
+}   
+}
+
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto Pesos){
+		resultado = monto * tipodecambio
+	var montopesos = document.getElementById('Monto pesos').value
+	peso.convertir('montopesos')
+	
+} 
+  }
+
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto Pesos){
+		resultado = monto * tipodecambio
+	var montopesos = document.getElementById('Monto pesos').value
+	libra.convertir('montopesos')
+	
+}   
+}
+
+
+
+
+
+var euro3 = new moneda()
+euro.nombre ="euro"
+euro.tipodecambio= 1.09
+
+var dolar3 = new moneda()
+dolar.nombre ="dolar"
+dolar.tipodecambio= 1.22
+var peso3 = new moneda()
+peso.nombre ="peso"
+peso.tipodecambio= 54.11
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto Libras){
+		resultado = monto * tipodecambio
+	var montolibras = document.getElementById('Monto libras').value
+	euro.convertir('montolibras')
+	
+}  
+ }
+
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto Libras){
+		resultado = monto * tipodecambio
+	var montolibras = document.getElementById('Monto libras').value
+	peso.convertir('montolibras')
+	
+}   
+}
+
+function convertirmoneda(){
+	this.nombre
+	this.tipodecambio
+	
+	this.convertirdolar = function(Monto Libras){
+		resultado = monto * tipodecambio
+	var montolibras = document.getElementById('Monto libras').value
+	dolar.convertir('montolibras')
+	
+} 
+}  
+
+
+
+
